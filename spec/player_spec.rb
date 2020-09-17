67041cc483cabc4703c87758386ac7606cd47afb
Yanick <tyankee15@gmail.com>
2020-09-17 13:19:01 -0300

    Test Player.make_move:
    Created test for method make_move Class Player created.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,5 +9,12 @@ describe Player do
       player = Player.new('Yanick', 'x')
       expect(player.check_position?(5, Board.new)).to eql(true)
     end
+ 
+    it 'return false if position is taken' do
+      player = Player.new('John', 'O')
+      board = Board.new
+      player.make_move(1, board)
+      expect(player.check_position?(1, board)).to eql(false)
+    end
   end
 end
