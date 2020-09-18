4f5a702023e69ab2053fb09dd09617dd92fb48bb
John Elvis Durán <jelduran@gmail.com>
2020-09-18 01:50:06 -0500

    Use let:
    Using let to define variables for the test cases.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,28 +4,26 @@ require './lib/player'
 require './lib/board'
 
 describe Board do
+  let(:board) { Board.new }
+
     describe '#current_board' do
       it 'returns an empty board' do
-        board = Board.new
         expect(board.current_board).to eql("\n   |   |   \n----------- \n   |   |   \n----------- \n   |   |   \n")
       end
     end
 
     describe '#reset' do
       it 'checks if all positions in board are empty after reset' do
-        board = Board.new
         board.move(0, 0, 'X')
         expect { board.reset }.to change(board, :positions).to [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
       end
 
       it 'checks if number of moves is zero after reset' do
-        board = Board.new
         board.move(0, 0, 'X')
         expect { board.reset }.to change(board, :move_number).to 0
       end
 
       it 'checks if winner is set to false after reset' do
-        board = Board.new
         board.move(0, 0, 'X')
         board.move(1, 0, 'O')
         board.move(0, 1, 'X')
@@ -37,7 +35,6 @@ describe Board do
 
     describe '#move' do
       it 'returns if there is a winner after the fourth move' do
-        board = Board.new
         board.move(0, 0, 'X')
         board.move(0, 2, 'O')
         board.move(1, 1, 'X')
@@ -48,35 +45,30 @@ describe Board do
 
     describe '#check_winner' do
       it 'checks if there is a winner in row' do
-        board = Board.new
         board.move(0, 0, 'X')
         board.move(0, 1, 'X')
         expect(board.check_winner(0, 2, 'X')).not_to eql(true)
       end
 
       it 'checks if there is a winner in column' do
-        board = Board.new
         board.move(0, 0, 'X')
         board.move(1, 0, 'X')
         expect(board.check_winner(2, 0, 'X')).not_to eql(true)
       end
 
       it 'checks if there is a winner in diagonal from left to right' do
-        board = Board.new
         board.move(0, 0, 'X')
         board.move(1, 1, 'X')
         expect(board.check_winner(2, 2, 'X')).not_to eql(true)
       end
 
       it 'checks if there is a winner in diagonal from right to left' do
-        board = Board.new
         board.move(0, 2, 'X')
         board.move(1, 1, 'X')
         expect(board.check_winner(2, 0, 'X')).not_to eql(true)
       end
 
       it 'checks if there is a draw' do
-        board = Board.new
         board.move(0, 0, 'X')
         board.move(0, 1, 'O')
         board.move(0, 2, 'X')
@@ -91,16 +83,13 @@ describe Board do
 
     describe '#empty_position?' do
       it 'returns true if position is empty' do
-        board = Board.new
         board.move(0, 0, 'X')
         expect(board.empty_position?(0, 1)).to eql(true)
       end
 
       it 'returns false if position is taken' do
-        board = Board.new
         board.move(1, 1, 'X')
         expect(board.empty_position?(1, 1)).to eql(false)
       end
     end
 end
-
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,25 +4,23 @@ require './lib/player'
 require './lib/board'
 
 describe Player do
+  let(:player1) { Player.new('Yanick', 'X') }
+  let(:player2) { Player.new('John', 'O') }
+  let(:board) { Board.new }
+  
   describe '#check_position?' do
     it 'return true if position is empty.' do
-      player = Player.new('Yanick', 'x')
-      expect(player.check_position?(5, Board.new)).to eql(true)
+      expect(player1.check_position?(5, Board.new)).to eql(true)
     end
  
     it 'return false if position is taken' do
-      player = Player.new('John', 'Or')
-      board = Board.new
-      player.make_move(1, board)
-      expect(player.check_position?(1, board)).to eql(false)
+      player1.make_move(1, board)
+      expect(player2.check_position?(1, board)).to eql(false)
     end
   end
 
   describe '#make_move' do
     it 'return true if there is a winner.' do
-      player1 = Player.new('Yanick', 'X')
-      player2 = Player.new('John', 'O')
-      board = Board.new
       player1.make_move(1, board)
       player2.make_move(3, board)
       player1.make_move(5, board)
